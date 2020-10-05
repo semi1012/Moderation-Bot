b0e876d4c430b580af63a62b461b16b8a7101daf
semi1012 <66850518+semi1012@users.noreply.github.com>
2020-10-05 21:42:25 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
-const Discord = require('discord.js');
+const Discord = require ('discord.js');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.categories = fs.readdirSync("./commands/");
 
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -11,25 +13,26 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
-    const config = require('./config.json')
 }
 
 client.on('ready', ()=> {
-    console.log(`${client.user.tag} is ready!`);
+    console.log(`${client.user.tag} has logged in and is ready`);
 });
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (!client.commands.has(command)) return;
 
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+    try {
+        client.commands.get(command).execute(client, message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command, please contact VapiX#9672 on Discord');
+    }
 });
+
+client.login(token);
